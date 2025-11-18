6e1f0fc2a988656e5352751a63e9abfe43881540
NH-AT12C-AT 若林 <nhs240037@gmail.com>
2025-11-18 12:35:49 +0900

    🚧 コンストラクタだけ置いとく

---
+++ HEW2026_Project/UIObject.cpp
@@ -0,0 +1,8 @@
+﻿/*********************************************************************
+ * \file   UIObject.cpp
+ * \brief  UIオブジェクトの基底クラス Base class of UI Object.
+ *
+ * \author AT12C-41 Kotetsu Wakabayashi
+ * \date   2025-11-18
+ *********************************************************************/
+#include "UIObject.h"
---
+++ HEW2026_Project/UIObject.h
@@ -0,0 +1,21 @@
+﻿/*********************************************************************
+ * \file   UIObject.h
+ * \brief  UIオブジェクトの基底クラス Base class of UI Object.
+ *
+ * \author AT12C-41 Kotetsu Wakabayashi
+ * \date   2025-11-18
+ *********************************************************************/
+#pragma once
+#include <string>
+
+class UIObject
+{
+public:
+	UIObject();
+	UIObject(std::string RelativeTexturePathFromTextureFolder);
+	UIObject(std::string RelativeTexturePathFromTextureFolder, float PositionX, float PositionY);
+	UIObject(std::string RelativeTexturePathFromTextureFolder, float PositionX, float PositionY, float Width, float Height);
+	~UIObject();
+	virtual void Update() {};
+	virtual void Draw() {};
+};

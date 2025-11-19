b418b44b35ba3bce64f7607181ed802b9a9af44f
nhs240037 <238187822+nhs240037@users.noreply.github.com>
2025-11-19 16:34:50 +0900

    🚧 mainベースのObjectUIを導入

---
+++ HEW2026_Project/UIObject.h
@@ -0,0 +1,39 @@
+﻿/*********************************************************************
+ * \file   UIObject.h
+ * \brief  UIオブジェクトの基底クラス Base class of UI Object.
+ *
+ * \author AT12C-41 Kotetsu Wakabayashi
+ * \date   2025-11-18
+ *********************************************************************/
+#pragma once
+#include <string>
+#include "Sprite.h"
+#include "Texture.h"
+#include <DirectXMath.h>
+
+class UIObject
+{
+public:
+	UIObject();
+	UIObject(std::string RelativeTexturePathFromTextureFolder);
+	UIObject(std::string RelativeTexturePathFromTextureFolder, float PositionX, float PositionY);
+	UIObject(std::string RelativeTexturePathFromTextureFolder, float PositionX, float PositionY, float Width, float Height);
+	UIObject(std::string RelativeTexturePathFromTextureFolder, float PositionX, float PositionY, float Width, float Height, float RotationX, float RotationY, float RotationZ);
+	~UIObject();
+	virtual void Update() {};
+	void Draw();
+
+	void SetPosition(float X, float Y);
+	void SetSize(float W, float H);
+	void SetRotation(float X, float Y, float Z);
+
+	DirectX::XMFLOAT2 GetPosition(void);
+	DirectX::XMFLOAT2 GetSize(void);
+	DirectX::XMFLOAT3 GetRotation(void);
+
+private:
+	Texture* m_pTexture;
+	DirectX::XMFLOAT2 m_fPosition;
+	DirectX::XMFLOAT2 m_fSize;
+	DirectX::XMFLOAT3 m_fRotation;
+};
